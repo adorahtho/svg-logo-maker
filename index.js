ec6557fe79ffe4bccd1ef470733bb76d3df5aac2
thoak90codes <adorahtho@gmail.com>
2023-05-11 22:17:40 -0500

    add classes to shapes.js file

--- index.js
+++ index.js
@@ -2,36 +2,36 @@ const inquirer = require('inquirer')
 const fs = require('fs')
 const shapes = require('./lib/shapes.js')
 
-class Logo {
-  constructor(shape, text){
-    this.shape = shape
-    this.text = text
+
+function askQuestions() {
+  return inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters for the logo.'
+      },
+      {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter a color keyword (OR a hexadecimal number) for the text.'
+      },
+      {
+        type: 'list',
+        name: 'shape',
+        message: 'Please select a shape.',
+        choices: ['circle', 'triangle', 'square']
+      },
+      {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter a color keyword (OR a hexadecimal number) for the shape color.',
+      },
+    ])
+    .then((response)=> {
+      console.log(response)
+    })
   }
-  askQuestions() {
-    return inquirer
-      .prompt([
-        {
-          type: 'input',
-          name: 'text',
-          message: 'Enter up to three characters for the logo.'
-        },
-        {
-          type: 'input',
-          name: 'textColor',
-          message: 'Enter a color keyword (OR a hexadecimal number) for the text.'
-        },
-        {
-          type: 'list',
-          name: 'shape',
-          message: 'Please select a shape.',
-          choices: ['circle', 'triangle', 'square']
-        },
-        {
-          type: 'input',
-          name: 'shapeColor',
-          message: 'Enter a color keyword (OR a hexadecimal number) for the shape color.',
-        },
-      ])
-  }
-}
+
+  askQuestions()
 
